94122c9f1b62c7cc48a3231fa8bac68556079680
danielV342 <17242199@esuda.edu.br>
2026-05-27 11:16:00 -0300

    Adição do arquivo de Documentação

--- conexao.php
+++ conexao.php
@@ -1,14 +1,18 @@
 <?php
+
 try {
+
     $pdo = new PDO(
-        "mysql:host=localhost;port=3307;dbname=cardapio;charset=utf8mb4",
+        "mysql:host=127.0.0.1;port=3306;dbname=cardapio;charset=utf8mb4",
         "root",
         ""
     );
 
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
+    echo "Conexão realizada com sucesso!";
+
 } catch (PDOException $e) {
-    die("Erro na conexão: " . $e->getMessage());
+
+
 }
-?>
